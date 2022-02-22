ebd290a5dd7273b43458bd5002457c5ca1bf8e1b
Jamez Mayke <jamezmayke@gmail.com>
2022-02-22 14:48:23 -0300

    Contrução dos dados de captura do cliente - post

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-const { response } = require('express');
+const { v4: uuidv4 } = require("uuid");
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
+const customers = [];
 
 app.get("/", (request, response) => {
   return response.json({
     "idName": "James"
   })
+})
+
+/**
+ * dados para criar:
+ * name - string
+ * cpf - string
+ * id - uuid
+ * statement - []
+ */
+app.post("/account", (request, response) => {
+  const { name, cpf } = request.body;
+  const id = uuidv4();
+
+  customers.push({ name, cpf, id, statement: []});
+
+  return response.status(201).send();
 })
 
 app.listen(1995, () => {
